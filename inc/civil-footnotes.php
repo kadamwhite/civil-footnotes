44c8cc5992ae574e4aff270a0632bf29fc429f08
K. Adam White <kadamwhite@users.noreply.github.com>
2019-08-11 20:38:50 -0400

    Refactor markup generation for legibility & efficiency.

--- inc/civil-footnotes.php
+++ inc/civil-footnotes.php
@@ -26,97 +26,95 @@ function setup() {
 function process_footnote( $content ) {
 	global $post;
 
-	// Regex extraction of all footnotes (or return if there are none)
-	if ( ! preg_match_all(
-		'/(?:\(\(|<footnote>)(.*)(?:\)\)|<\/footnote>)/Us',
-		$content,
-		$identifiers,
-		PREG_SET_ORDER
-	) ) {
+	// Derive values necessary for link generation. Use full permalink in feed context.
+	$permalink = is_feed() ? get_permalink( $post->ID ) : '';
+	$post_id   = $post->ID;
+
+	// Detect & process footnotes within the page content.
+	$footnotes       = [];
+	$footnote_number = 1;
+	$mutated_content = preg_replace_callback(
+		// Match footnotes surrounded by `(())` or by `<footnote></footnote>`.
+		'/\s?(?:\(\(|<footnote>)(.*)(?:\)\)|<\/footnote>)/Us',
+		/**
+		 * For each regex match, replace the footnote placeholder with a <sup> tag
+		 * and store footnote information for list generation below.
+		 * @param array $match The regex match.
+		 * @return string The replacement string.
+		 */
+		function( $match ) use ( &$footnotes, &$footnote_number, $permalink, $post_id ) {
+			// Store footnote content so we can generate the list at the end of the post.
+			$footnote = [
+				'content' => $match[1],
+				'fn_id'   => sprintf( 'fn%d-%d', $footnote_number, $post_id ),
+				'ref_id'  => sprintf( 'rf%d-%d', $footnote_number, $post_id ),
+				'number'  => $footnote_number,
+				'symbol'  => Formats\format( $footnote_number ),
+			];
+
+			// Derive the href properties from the appropriate IDs.
+			$footnote['fn_href']  = sprintf( '%s#%s', $permalink, $footnote['fn_id'] );
+			$footnote['ref_href'] = sprintf( '%s#%s', $permalink, $footnote['ref_id'] );
+
+			// Store footnote in footnotes array.
+			$footnotes[] = $footnote;
+
+			// Increment the current footnote number in preparation for subsequent notes.
+			$footnote_number++;
+
+			// Replace the footnote placeholder with the superscript indicator.
+			return sprintf(
+				'<sup id="%1$s"><a href="%2$s" title="%3$s" rel="footnote">%4$s</a></sup>',
+				esc_attr( $footnote['ref_id'] ),
+				esc_attr( $footnote['fn_href'] ),
+				esc_attr( $footnote['content'] ),
+				esc_html( $footnote['symbol'] )
+			);
+		},
+		$content
+	);
+
+	// If we do not believe any transformation has occurred, take no further action.
+	if ( 1 === $footnote_number || $content === $mutated_content ) {
 		return $content;
 	}
 
-	$footnotes = array();
-
 	// Create 'em
-	$identifier_count = count( $identifiers );
-	for ( $i = 0; $i < $identifier_count; $i++ ) {
-		// Look for ref: and replace in identifiers array.
-		if ( substr( $identifiers[ $i ][1], 0, 4 ) === 'ref:' ) {
-			$ref                       = (int) substr( $identifiers[ $i ][1], 4 );
-			$identifiers[ $i ]['text'] = $identifiers[ $ref - 1 ][1];
-		} else {
-			$identifiers[ $i ]['text'] = $identifiers[ $i ][1];
-		}
-
-		if ( ! isset( $identifiers[ $i ]['use_footnote'] ) ) {
-			// Add footnote and record the key
-			$identifiers[ $i ]['use_footnote']                                  = count( $footnotes );
-			$footnotes  [ $identifiers[ $i ]['use_footnote'] ]['text']          = $identifiers[ $i ]['text'];
-			$footnotes  [ $identifiers[ $i ]['use_footnote'] ]['symbol']        = ( array_key_exists( 'symbol', $identifiers[ $i ] ) ) ?
-				$identifiers[ $i ]['symbol'] :
-				'';  // Bugfix submitted by Greg Sullivan
-			$footnotes  [ $identifiers[ $i ]['use_footnote'] ]['identifiers'][] = $i;
-		}
-	}
-
-	// Footnotes and identifiers are stored in the array
-
-	$use_full_link = false;
-	if ( is_feed() ) {
-		$use_full_link = true;
-	}
-
-	if ( is_preview() ) {
-		$use_full_link = false;
-	}
-
-	// Display identifiers
-	$datanote = ''; // Bugfix submitted by Greg Sullivan
-	foreach ( $identifiers as $key => $value ) {
-
-		$id_num     = $value['use_footnote'] + 1;
-		$id_id      = 'rf' . $id_num . '-' . $post->ID;
-		$id_href    = ( ( $use_full_link ) ? get_permalink( $post->ID ) : '' ) . '#fn' . $id_num . '-' . $post->ID;
-		$id_title   = str_replace( '"', '&quot;', htmlentities( html_entity_decode( wp_strip_all_tags( $value['text'] ), ENT_QUOTES, 'UTF-8' ), ENT_QUOTES, 'UTF-8' ) );
-		$id_replace = sprintf(
-			'<sup id="%1$s"><a href="%2$s" title="%3$s" rel="footnote">%4$s</a></sup>',
-			esc_attr( $id_id ),
-			esc_attr( $id_href ),
-			esc_attr( $id_title ),
-			esc_html( Formats\format( $id_num ) )
-		);
-
-		$content = substr_replace( $content, $id_replace, strpos( $content, $value[0] ), strlen( $value[0] ) );
-
-		// Display the footnotes (here is where you can change the output)
-
-		// Create each footnote
-		$datanote = $datanote . sprintf(
-			'<li id="fn%1$d-%2$d">' .
-			'<p>%3$s&nbsp;' .
-			'<a href="#%4$s" class="backlink" title="%5$s">&#8617;</a>' .
-			'</p></li>',
-			// %1$d: Number of the footnote being rendered (1-indexed).
-			esc_attr( $id_num ),
-			// %2$d: ID of the post being rendered.
-			$post->ID,
-			// %3$s: Footnote content.
-			esc_html( $value['text'] ),
-			// %4$s: HTML ID attribute of the footnote being rendered.
-			esc_attr( $id_id ),
-			// %5$s: User-facing messaging for the return link.
-			sprintf(
-				/* translators: %d: The number of the footnote to which to return. */
-				__( 'Return to footnote %d.', 'civil-footnotes' ),
-				esc_attr( $id_num )
-			)
-		);
-	}
+	$footnote_li_tags = array_reduce(
+		$footnotes,
+		/**
+		 * Generate a rendered <li> item string for each footnote.
+		 *
+		 * @param string $li_tags  String to which we will concatenate each <li> tag.
+		 * @param array  $footnote Footnote item from preg_replace_callback, above.
+		 * @return string A rendered <li> string for this footnote.
+		 */
+		function( string $li_tags, array $footnote ) : string {
+			return $li_tags . sprintf(
+				'<li id="%1$s">' .
+				'<p>%2$s' .
+				'&nbsp;<a href="%3$s" class="backlink" title="%4$s">&#8617;</a>' .
+				'</p></li>',
+				// %1$s: HTML ID attribute of the footnote <li> being rendered.
+				esc_attr( $footnote['fn_id'] ),
+				// %2$s: Footnote content.
+				esc_html( $footnote['content'] ),
+				// %3$s: HTML href attribute pointing back to the footnote <sup> tag.
+				esc_html( $footnote['ref_href'] ),
+				// %4$s: User-facing messaging for the return link.
+				sprintf(
+					/* translators: %s: The symbol or number of the footnote to which to return. */
+					__( 'Return to footnote %s.', 'civil-footnotes' ),
+					esc_attr( $footnote['symbol'] )
+				)
+			);
+		},
+		''
+	);
 
 	// Create the footnotes
-	return $content . sprintf(
+	return $mutated_content . sprintf(
 		'<hr class="footnotes"><ol class="footnotes">%s</ol>',
-		$datanote
+		$footnote_li_tags
 	);
 }
