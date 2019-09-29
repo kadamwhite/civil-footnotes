707c982c3a81def75ff3a88d7f5b304a24290fe2
K. Adam White <kadamwhite@users.noreply.github.com>
2019-09-29 12:17:15 -0400

    Resolve PHPCS errors in version warning code

--- civil-footnotes.php
+++ civil-footnotes.php
@@ -9,21 +9,24 @@ Author: <a href="https://defomicron.net/colophon">Austin Sweeney</a>
 
 // We only support PHP 7 and up.
 if ( version_compare( phpversion(), '7.0.0', '<' ) ) {
-	add_action( 'admin_notices', function() {
-		/* translators: Error message explaining PHP version is too low. %s: Current PHP version. */
-		$php_version_warning = sprintf(
-			__( 'Civil Footnotes requires PHP 7 or higher; you are using %s. Please contact your host to upgrade your PHP version, or downgrade to Civil Footnotes version 1.3.1.' ),
-			phversion()
-		);
-		$deactivation_notice = __( 'The plugin has been deactivated.', 'civil-footnotes' );
+	add_action(
+		'admin_notices',
+		function() {
+			$php_version_warning = sprintf(
+				/* translators: Error message explaining PHP version is too low. %s: Current PHP version. */
+				__( 'Civil Footnotes requires PHP 7 or higher; you are using %s. Please contact your host to upgrade your PHP version, or downgrade to Civil Footnotes version 1.3.1.' ),
+				phversion()
+			);
+			$deactivation_notice = __( 'The plugin has been deactivated.', 'civil-footnotes' );
 
-		?>
+			?>
 		<div class="notice notice-error is-dismissible">
 			<p><?php esc_html( $php_version_warning ); ?></p>
 			<p><?php esc_html( $deactivation_notice ); ?></p>
 		</div>
-		<?php
-	} );
+			<?php
+		}
+	);
 
 	deactivate_plugins( plugin_basename( __FILE__ ) );
 
