398ed459932e412cdc7ba2811e6783b8717a8027
K. Adam White <kadamwhite@users.noreply.github.com>
2019-09-29 12:49:39 -0400

    Fix deactivation logic and correct logic errors

--- civil-footnotes.php
+++ civil-footnotes.php
@@ -15,20 +15,25 @@ if ( version_compare( phpversion(), '7.0.0', '<' ) ) {
 			$php_version_warning = sprintf(
 				/* translators: Error message explaining PHP version is too low. %s: Current PHP version. */
 				__( 'Civil Footnotes requires PHP 7 or higher; you are using %s. Please contact your host to upgrade your PHP version, or downgrade to Civil Footnotes version 1.3.1.' ),
-				phversion()
+				phpversion()
 			);
 			$deactivation_notice = __( 'The plugin has been deactivated.', 'civil-footnotes' );
 
 			?>
 		<div class="notice notice-error is-dismissible">
-			<p><?php esc_html( $php_version_warning ); ?></p>
-			<p><?php esc_html( $deactivation_notice ); ?></p>
+			<p><?php echo esc_html( $php_version_warning ); ?></p>
+			<p><?php echo esc_html( $deactivation_notice ); ?></p>
 		</div>
 			<?php
 		}
 	);
 
-	deactivate_plugins( plugin_basename( __FILE__ ) );
+	add_action(
+		'admin_init',
+		function() {
+			deactivate_plugins( plugin_basename( __FILE__ ) );
+		}
+	);
 
 	// Halt plugin initialization.
 	return;
